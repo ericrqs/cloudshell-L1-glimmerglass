079bec141af06cdaadb89286a0acfdee56c25e1b
CoYe <wiseman.kostya@gmail.com>
2016-06-14 01:21:50 +0300

    initial commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pkgutil import extend_path
+path = extend_path(__path__, __name__)
---
+++ compile_driver.bat
@@ -0,0 +1,1 @@
+C:\ProgramData\QualiSystems\QsPython27\Scripts\pyinstaller --onefile "C:\_Quali_Git\cloudshell-L1-glimmerglass\driver.spec"
